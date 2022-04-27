f5570b35ca4e812af51140b24ab8d39502ebb5c1
Hannah <hannahmgrout@gmail.com>
2022-04-26 19:23:06 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,26 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  function getToys(){
+    fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(toysArr => toysArr.forEach(toy => renderToy(toy)))
+  }
+
+  function renderToy(toy){
+    let toyCard = document.createElement('div')
+    toyCard.classList.add('card')
+    toyCard.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}">
+      <p>${toy.likes} ' likes'<p>
+      <button class="like-btn" id"${toy.id}">Like ❤️</button>
+      </div>
+    `
+    toyFormContainer.appendChild(toyCard)
+  }
+
+
+getToys()
 });
